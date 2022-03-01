20ce93d852dd821d7d5b572b520b473797fc5ee9
CHO1116 <dudwnscho@naver.com>
2022-03-01 23:08:32 +0900

    rewrite server.js

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ import express from "express";
 
 const app = express();
 
-console.log("Hello");
+const handleListen = () => console.log('Listening on http://localhost:3000');
 
-app.listen(3000);
+app.listen(3000, handleListen);
